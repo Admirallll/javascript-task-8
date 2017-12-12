d73ccf7f3dfa56a6406c4c44bcc6722464b25830
Admirallll <stanislav.yarkeev@gmail.com>
2017-12-12 21:39:17 +0500

    initial work

--- client-core.js
+++ client-core.js
@@ -1,11 +1,102 @@
 'use strict';
 
 module.exports.execute = execute;
-module.exports.isStar = true;
+module.exports.isStar = false;
+const request = require('request');
+const chalk = require('chalk');
+const red = chalk.hex('#f00');
+const green = chalk.hex('#0f0');
 
 function execute() {
-    // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
+    const { from, to, text, command } = parseArgs();
+    if (command === 'list') {
+        return listRequest(from, to);
+    } else if (command === 'send') {
+        return sendRequest({ from, to, text });
+    }
+}
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+function listRequest(from, to) {
+    const url = 'http://localhost:8080/messages';
+
+    return new Promise((resolve, reject) => {
+        request.get({ uri: url, qs: createDefinedQueryParams(from, to) })
+            .on('response', res => {
+                let body = [];
+                res.on('data', function (chunk) {
+                    body.push(chunk);
+                });
+
+                res.on('end', function () {
+                    body = Buffer.concat(body).toString();
+                    let result = JSON.parse(body);
+                    result = result.map(messageToConsoleString);
+                    resolve(result.join('\n\n'));
+                });
+            })
+            .on('error', reject);
+    });
+}
+
+function sendRequest(message) {
+    const { from, to, text } = message;
+
+    return new Promise((resolve, reject) => {
+        let req = request
+            .post({ uri: 'http://localhost:8080/messages', qs: createDefinedQueryParams(from, to) })
+            .on('response', res => {
+                let body = [];
+                res.on('data', function (chunk) {
+                    body.push(chunk);
+                });
+
+                res.on('end', function () {
+                    body = Buffer.concat(body).toString();
+                    let result = JSON.parse(body);
+                    resolve(messageToConsoleString(result));
+                });
+            });
+        req.on('error', function (e) {
+            reject(e);
+        });
+
+        req.write(JSON.stringify({ text }));
+        req.end();
+    });
+}
+
+function createDefinedQueryParams(from, to) {
+    let result = {};
+    if (from) {
+        result.from = from;
+    }
+    if (to) {
+        result.to = to;
+    }
+
+    return result;
+}
+
+function parseArgs() {
+    var ArgumentParser = require('argparse').ArgumentParser;
+    var parser = new ArgumentParser();
+    parser.addArgument('command');
+    parser.addArgument('--from');
+    parser.addArgument('--to');
+    parser.addArgument('--text');
+
+    return parser.parseArgs();
+}
+
+function messageToConsoleString(message) {
+    let strings = [];
+    if (message.from !== undefined) {
+        strings.push(`${red('FROM')}: ${message.from}`);
+    }
+    if (message.to !== undefined) {
+        strings.push(`${red('TO')}: ${message.to}`);
+    }
+    strings.push(`${green('TEXT')}: ${message.text}`);
+
+    return strings.join('\n');
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,61 @@
 'use strict';
 
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer();
 
+let messages = [];
+
 server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
+    console.info(req.url);
+    const { method } = req;
+    const { pathname, query } = url.parse(req.url, true);
+    if (pathname === '/messages') {
+        let result = {};
+        if (method === 'GET') {
+            result = getMessages(query.from, query.to);
+            console.info(result);
+            sendJsonResultWithMessage(res, result);
+        } else {
+            let body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                body = JSON.parse(body);
+                console.info(body);
+                result = putMessage({ text: body.text, from: query.from, to: query.to });
+                console.info(result);
+                sendJsonResultWithMessage(res, result);
+            });
+        }
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
 });
+
+function sendJsonResultWithMessage(result, message) {
+    result.setHeader('Content-Type', 'application/json');
+    result.statusCode = 200;
+    result.write(JSON.stringify(message));
+    result.end();
+}
+
+function putMessage(message) {
+    messages.push(message);
+
+    return message;
+}
+
+function getMessages(from, to) {
+    let result = messages.filter(message =>
+        (from === undefined || message.from === from) &&
+        (to === undefined || message.to === to)
+    );
+
+    return result;
+}
 
 module.exports = server;
